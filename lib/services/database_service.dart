30987112198a0435ddb8bbbce5dbe0c73681ac92
Sanjide <sanjidmsnjdp@gmail.com>
2026-03-17 19:27:54 +0530

    Update database_service.dart

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -4,25 +4,28 @@ import 'package:hive_flutter/hive_flutter.dart';
 import '../core/models/app_models.dart';
 
 class DatabaseService {
-  // ബോക്സുകളുടെ പേരുകൾ (Table Names)
+  // ബോക്സുകളുടെ പേരുകൾ
   static const String prayersBoxName = 'prayersBox';
   static const String defaultPrayersBoxName = 'defaultPrayersBox';
   static const String dhikrsBoxName = 'dhikrsBox';
   static const String notesBoxName = 'notesBox';
+  static const String settingsBoxName = 'settingsBox'; // സെറ്റിംഗ്സിനായി പുതിയ ബോക്സ്
 
-  // ഡാറ്റാബേസ് ഇനിഷ്യലൈസ് ചെയ്യാനുള്ള ഫംഗ്ഷൻ (main.dart ൽ വിളിക്കാൻ)
   static Future<void> initDatabase() async {
     await Hive.initFlutter();
 
-    // മോഡലുകൾ ഡാറ്റാബേസിനെ പരിചയപ്പെടുത്തുന്നു
+    // മോഡലുകൾ പരിചയപ്പെടുത്തുന്നു
     Hive.registerAdapter(CustomPrayerAdapter());
     Hive.registerAdapter(CustomDhikrAdapter());
     Hive.registerAdapter(NoteItemAdapter());
 
     // ബോക്സുകൾ ഓപ്പൺ ചെയ്യുന്നു
     await Hive.openBox<CustomPrayer>(prayersBoxName);
-    await Hive.openBox<Map>(defaultPrayersBoxName); // 5 വഖ്ത് സേവ് ചെയ്യാൻ
+    await Hive.openBox<Map>(defaultPrayersBoxName); 
     await Hive.openBox<CustomDhikr>(dhikrsBoxName);
     await Hive.openBox<NoteItem>(notesBoxName);
+    
+    // ഡാർക്ക് മോഡും ഭാഷകളും സേവ് ചെയ്യാൻ ഒരു ജനറൽ ബോക്സ് ഓപ്പൺ ചെയ്യുന്നു
+    await Hive.openBox(settingsBoxName); 
   }
 }
