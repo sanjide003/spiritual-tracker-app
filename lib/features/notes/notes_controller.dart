91f5e3a0e868ad05fc4bc4b9e957f3758e06b635
Sanjide <sanjidmsnjdp@gmail.com>
2026-03-17 17:43:50 +0530

    Update notes_controller.dart

--- lib/features/notes/notes_controller.dart
+++ lib/features/notes/notes_controller.dart
@@ -3,22 +3,29 @@
 import 'package:flutter/material.dart';
 
 class NoteItem {
+  String id;      // പുതിയതായി ആഡ് ചെയ്തു
   String title;
   String content;
+  String type;    // പുതിയതായി ആഡ് ചെയ്തു (text, image, pdf)
 
-  NoteItem({required this.title, required this.content});
+  NoteItem({required this.id, required this.title, required this.content, required this.type});
 }
 
 class NotesController extends ChangeNotifier {
   final List<NoteItem> notes = [];
 
-  void addNote(String title, String content) {
-    notes.add(NoteItem(title: title, content: content));
+  void addNote(String title, String type, String content) {
+    notes.add(NoteItem(
+      id: DateTime.now().toString(), 
+      title: title, 
+      type: type, 
+      content: content
+    ));
     notifyListeners();
   }
 
-  void deleteNote(int index) {
-    notes.removeAt(index);
+  void deleteNote(String id) {
+    notes.removeWhere((note) => note.id == id);
     notifyListeners();
   }
 }
